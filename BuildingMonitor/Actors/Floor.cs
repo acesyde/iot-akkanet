a14d5339cbd875bdf38285cf72d4ad208ff7ec7e
Pierre-Emmanuel Mercier <pierreemmanuel.mercier@orange.com>
2018-04-06 21:42:38 +0200

    Create / Group / Supervising temperature sensor

---
+++ BuildingMonitor/Actors/Floor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Akka.Actor;
+using BuildingMonitor.Messages;
+
+namespace BuildingMonitor.Actors
+{
+    public class Floor : UntypedActor
+    {
+        private readonly string _floorId;
+        private readonly Dictionary<string, IActorRef> _sensorIdToActorRefs = new Dictionary<string, IActorRef>();
+
+        public Floor(string floorId)
+        {
+            _floorId = floorId;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case RequestRegisterTemperatureSensor m when m.FloorId == _floorId:
+                    if(_sensorIdToActorRefs.TryGetValue(m.SensorId, out var existingSensorActorRef))
+                        existingSensorActorRef.Forward(m);
+
+                    var newSensorActor = Context.ActorOf(TemperatureSensor.Props(_floorId, m.SensorId), $"temperature-sensor-{m.SensorId}");
+                    Context.Watch(newSensorActor);
+                    _sensorIdToActorRefs.Add(m.SensorId, newSensorActor);
+                    newSensorActor.Forward(m); 
+                    break;
+                case RequestTemperatureSensorIds m:
+                    Sender.Tell(new RespondTemperatureSensorIds(m.RequestId, new HashSet<string>(_sensorIdToActorRefs.Keys)));
+                    break;
+                case Terminated m:
+                    var terminatedTemperatureSensorId = _sensorIdToActorRefs.First(x => x.Value == m.ActorRef).Key;
+                    _sensorIdToActorRefs.Remove(terminatedTemperatureSensorId);
+                    break;
+                default:
+                    Unhandled(message);
+                    break;
+            }
+        }
+
+        public static Props Props(string floorId) => Akka.Actor.Props.Create(() => new Floor(floorId));
+    }
+}
