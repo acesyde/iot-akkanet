8b0bceb7e0bd508930d1578614912c228b371993
Pierre-Emmanuel Mercier <pierreemmanuel.mercier@orange.com>
2018-04-08 20:43:02 +0200

    add query

---
+++ BuildingMonitor/Actors/FloorQuery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Akka.Actor;
+
+namespace BuildingMonitor.Actors
+{
+    public class FloorQuery : UntypedActor
+    {
+        private readonly Dictionary<IActorRef, string> _actorToSensorId;
+        private readonly int _requestId;
+        private readonly IActorRef _requester;
+        private readonly TimeSpan _timeout;
+        public static readonly long TemperatureRequestCorrelationId = 42;
+
+        public FloorQuery(Dictionary<IActorRef, string> actorToSensorId, int requestId, IActorRef requester, TimeSpan timeout)
+        {
+            _actorToSensorId = actorToSensorId;
+            _requestId = requestId;
+            _requester = requester;
+            _timeout = timeout;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Props Props(Dictionary<IActorRef, string> actorToSensorId, int requestId, IActorRef requester, TimeSpan timeout)
+            => Akka.Actor.Props.Create(() => new FloorQuery(actorToSensorId, requestId, requester, timeout));
+    }
+}
---
+++ BuildingMonitor/Messages/RespondAllTemperatures.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace BuildingMonitor.Messages
+{
+    public sealed class RespondAllTemperatures
+    {
+        public long RequestId { get; }
+        public IImmutableDictionary<string, ITemperatureQueryReading> TemperatureReadings { get; }
+
+        public RespondAllTemperatures(long requestId, IImmutableDictionary<string, ITemperatureQueryReading> temperatureReadings)
+        {
+            RequestId = requestId;
+            TemperatureReadings = temperatureReadings;
+        }
+    }
+}
